f3795da22df2b3e033fae8791698e4c6b9b8aad8
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-09 10:14:22 +0500

    extends'

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 require_once ('../vendor/autoload.php');
-$worker = new \App\Worker('vasya', 33, [5, 7, 10]);
-$visitor = new \App\Visitor();
 
-var_dump($worker);
+
+$developer = new \App\Developer('vasya', 33, [5, 7, 10]);
+
+$developer->work();
 
