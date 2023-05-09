e9e0193a8f0cd921e3e3157881d3857f6201f47a
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-09 19:46:13 +0500

    ALT+Insert select parametrs

--- app/Worker.php
+++ app/Worker.php
@@ -22,19 +22,89 @@ class Worker
         $this->hours = $hours;
     }
 
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param int $age
+     */
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHours(): array
+    {
+        return $this->hours;
+    }
+
+    /**
+     * @param array $hours
+     */
+    public function setHours(array $hours): void
+    {
+        $this->hours = $hours;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPosition(): string
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param string $position
+     */
+    public function setPosition(string $position): void
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @return int
+     */
+    public function getExperience(): int
+    {
+        return $this->experience;
+    }
+
+    /**
+     * @param int $experience
+     */
+    public function setExperience(int $experience): void
+    {
+        $this->experience = $experience;
+    }
 
     public function work()
     {
         print_r('I am worker');
     }
 
-    public function setPosition($value)
-    {
-        $this->position = $value;
-    }
-
-    public function getPosition(): string
-    {
-        return $this->position;
-    }
 }
