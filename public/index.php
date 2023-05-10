fb63aa290896a014c973244c4095da6ab59c5551
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-10 13:16:27 +0500

    get_class

--- public/index.php
+++ public/index.php
@@ -6,4 +6,4 @@ $developer = new \App\Developer('Boris ', 20, [5, 6, 10]);
 $designer = new \App\Designer('Elena ', 18, [5, 6, 10]);
 
 
-var_dump(get_class_vars('App\Developer'));
+var_dump(get_class($developer));
